68538efd5d1ed12f6f3b2971d0e6fbfcfa1a7716
Phuc2302 <77436102+Phuc2302@users.noreply.github.com>
2021-11-03 15:26:06 +0700

    change main

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_catalog/core/store.dart';
 import 'package:flutter_catalog/pages/Welcome/welcome_screen.dart';
 import 'package:flutter_catalog/pages/cart_page.dart';
-import 'package:flutter_catalog/pages/login_page.dart';
 import 'package:flutter_catalog/utils/routes.dart';
 import 'package:flutter_catalog/widgets/themes.dart';
 import 'package:velocity_x/velocity_x.dart';
@@ -26,13 +25,10 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.light,
       theme: MyTheme.lightTheme(context),
       darkTheme: MyTheme.darkTheme(context),
-      // initialRoute: MyRoutes.homeRoute,
       initialRoute: "/",
       routes: {
-        // "/": (context) => LoginPage(),
         "/": (context) => WelcomeScreen(),
         MyRoutes.homeRoute: (context) => HomePage(),
-        MyRoutes.loginRoute: (context) => LoginPage(),
         MyRoutes.cartRoute: (context) => CartPage()
       },
     );
